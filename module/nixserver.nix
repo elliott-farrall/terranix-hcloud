3d6fe3044f2b7e6213dafaba18b50f8850383f38
elliott-farrall <108588212+elliott-farrall@users.noreply.github.com>
2025-08-01 08:19:15 +0100

    Remove direct dependency on nixos-infect
    Use `curl` to download nixos-infect to the target instead of using the provisioner to transfer the script. This makes it possible to use a remote backend.
    Also removed ability to upload a configuration file for compatibility with remote backend.

--- module/nixserver.nix
+++ module/nixserver.nix
@@ -2,13 +2,6 @@
 with lib;
 let
   cfg = config.hcloud.nixserver;
-
-  nixosInfect = pkgs.fetchgit {
-    "url" = "https://github.com/elitak/nixos-infect.git";
-    "rev" = "30441b1236a3f5e426b72ecf2ba2f4f91127ca0e";
-    "sha256" = "0k6z4wx8k03psbhm2mzm575z6gas2c6fl2pxpppwamjdvl0gv2an";
-  };
-
 in
 {
 
@@ -51,27 +44,11 @@ in
               enable backups or not
             '';
           };
-          configurationFile = mkOption {
-            default = null;
-            type = with types; nullOr path;
-            description = ''
-              The configuration.nix,
-              only used by the initial
-              provisioning by nixos-infect.
-            '';
-          };
           location = mkOption {
             default = null;
             type = nullOr str;
             description = ''
               location where the machine should run.
-            '';
-          };
-          provisioners = mkOption {
-            default = [ ];
-            type = with types; listOf attrs;
-            description = ''
-              provision steps. see `hcloud.server.provisioners`.
             '';
           };
           extraConfig = mkOption {
@@ -92,26 +69,13 @@ in
         name = "${configuration.name}";
         value = {
           inherit (configuration) enable serverType backups name location extraConfig;
-          provisioners = [
-            {
-              file.source = "${nixosInfect}/nixos-infect";
-              file.destination = "/root/nixos-infect";
-            }
-            (optionalAttrs (configuration.configurationFile != null) {
-              file.source = toString configuration.configurationFile;
-              file.destination = "/etc/nixos_input.nix";
-            })
-          ] ++ configuration.provisioners ++ [{
+          provisioners = [{
             remote-exec.inline = [
               ''
                 NO_REBOOT="dont" \
                 PROVIDER=HCloud \
                 NIX_CHANNEL=${configuration.channel} \
-                ${
-                  optionalString (configuration.configurationFile != null)
-                  "NIXOS_IMPORT=/etc/nixos_input.nix"
-                } \
-                bash /root/nixos-infect 2>&1 | tee /tmp/infect.log
+                curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | bash 2>&1 | tee /tmp/infect.log
               ''
               "shutdown -r +1"
             ];
